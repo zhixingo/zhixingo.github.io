43ebc57cbd682adbd883718fae1b0320bf0b79b4
muggledy <zgjsycfndy2015@163.com>
2022-01-03 16:04:47 +0800

    replace IP_Geolocation_API with https://iper.vercel.app/api/

--- js/get_ip.js
+++ js/get_ip.js
@@ -1,27 +1,28 @@
-//获取ip信息
-//调用IP Geolocation API接口，官网：http://ip-api.com/
-var ip_url = "http://ip-api.com/json/?callback=show_ip";
-function show_ip(data){
-    var hid7 = document.getElementById("hid7");
-    var p = hid7.getElementsByTagName("p")[0];
-    var ul = document.createElement("ul");
-    ul.className = "ul";
-    hid7.insertBefore(ul, p);
-    hid7.removeChild(p);
-    var items = ["query", "city", "timezone"];
-    var names = ["IP地址", "所在城市", "时区"];
-    for (var i = 0; i < items.length; ++i){
-        var li = document.createElement("li");
-        var info;
-        if(items[i] == "city"){
-            info = "<a href='https://www.google.com/search?q=" + data[items[i]] + "' target=\"_blank\">" + data[items[i]] +"</a>";
-        }else{
-            info = data[items[i]];
+//接口说明见https://github.com/muggledy/iper
+
+var hid7 = document.getElementById("hid7");
+var hid7_p = hid7.getElementsByTagName("p")[0];
+
+fetch("https://iper.vercel.app/api/index").then(function(data){
+        return data.text();
+    }).then(function(data){
+        data = eval("(" + data + ")");
+        var ul = document.createElement("ul");
+        ul.className = "ul";
+        hid7.insertBefore(ul, hid7_p);
+        hid7.removeChild(hid7_p);
+        var items = ["query", "city", "timezone"];
+        var names = ["IP地址", "所在城市", "时区"];
+        for (var i = 0; i < items.length; ++i){
+            var li = document.createElement("li");
+            var info;
+            if(items[i] == "city"){
+                info = "<a href='https://www.google.com/search?q=" + data[items[i]] + "' target=\"_blank\">" +
+                data[items[i]] +"</a>";
+            }else{
+                info = data[items[i]];
+            }
+            li.innerHTML = names[i] + ": " + info;
+            ul.appendChild(li);
         }
-        li.innerHTML = names[i] + ": " + info;
-        ul.appendChild(li);
-    }
-}
-var ip_script = document.createElement("script");
-ip_script.setAttribute("src", ip_url);
-document.getElementsByTagName("head")[0].appendChild(ip_script);
+}).catch(error => hid7_p.innerHTML = "IP获取失败，请检查网络！"); //https://juejin.cn/post/6844904193996619784
